4a4a7a6e8c0c893a02e8406bef645be8af67fd53
Sa3eedDev <36833212+sa3eedDev@users.noreply.github.com>
2023-04-02 03:19:45 -0400

    Changed the name

--- launch.sh
+++ launch.sh
@@ -8,5 +8,6 @@ LD_PRELOAD=/mnt/SDCARD/miyoo/lib/libpadsp.so /mnt/SDCARD/.tmp_update/bin/infoPan
 pkill -9 wpa_supplicant
 pkill -9 udhcpc
 pkill -9 filebrowser
+/mnt/SDCARD/App/FileBrowser/filebrowser config set --branding.name "Onion"
 /mnt/SDCARD/App/FileBrowser/filebrowser config set --branding.files "/mnt/SDCARD/App/FileBrowser/theme"
 /mnt/SDCARD/App/FileBrowser/filebrowser -p 80 -a $IP  -r /mnt/SDCARD &
